84abf7db5f0256daace4b67793dd1c96dfb285cb
NipunSanjaya <you@example.com>
2024-05-28 04:02:43 +0530

    create header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import Logo from './Logo'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='h-16 shadow-md'>
+        <div className='h-full container mx-auto flex items-center px-4 justify-between'>
+            <div className=''>
+                <Logo w={90} h={50}/>
+            </div>
+
+            <div>
+              <input type='text' placeholder='search product here...'/>
+            </div>
+
+            <div>
+              user icons and card
+            </div>
+        </div>
+    </header>
   )
 }
 
